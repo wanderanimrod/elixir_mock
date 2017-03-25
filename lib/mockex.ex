fc5ed81f24e1905d240f11571cdcb926d09dae0b
Nimrod Wandera <wanderanimrod@gmail.com>
2017-03-24 21:08:24 -0400

    starting out afresh

---
+++ lib/mockex.ex
@@ -0,0 +1,18 @@
+defmodule Mockex do
+  @moduledoc """
+  Documentation for Mockex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mockex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mockex_test.exs
@@ -0,0 +1,8 @@
+defmodule MockexTest do
+  use ExUnit.Case
+  doctest Mockex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
