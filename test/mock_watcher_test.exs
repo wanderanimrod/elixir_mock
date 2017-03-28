b57e433f5c0592bb2e611fb2867b646efb18d7f5
Nimrod Wandera <wanderanimrod@gmail.com>
2017-03-27 21:29:12 -0500

    Covers mock watcher with tests

---
+++ test/mock_watcher_test.exs
@@ -0,0 +1,37 @@
+defmodule MockWatcherTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    mock_name = :"#{UUID.uuid4(:hex)}"
+    watcher_name = MockWatcher.get_watcher_name_for(mock_name)
+    {:ok, %{mock: mock_name, watcher: watcher_name}}
+  end
+
+  test "should start process with given mock name", %{mock: mock} do
+    {:ok, _} = MockWatcher.start_link(mock)
+    assert is_pid(Process.whereis(:"__mockex__watcher_#{mock}"))
+  end
+  test "should verify that call does not exist when there are no calls on watcher", %{mock: mock, watcher: watcher} do
+    {:ok, _} = MockWatcher.start_link(mock)
+    assert not GenServer.call(watcher, {:call_exists, :fn_name, [:arg]})
+  end
+
+  test "should record and verify that call with correct args exists", %{mock: mock, watcher: watcher} do
+    {:ok, _} = MockWatcher.start_link(mock)
+    :ok = GenServer.call(watcher, {:record_call, :fn_name, [1, 2]})
+    assert GenServer.call(watcher, {:call_exists, :fn_name, [1, 2]})
+  end
+
+  test "should record and verify that call with wrong args does not exist", %{mock: mock, watcher: watcher} do
+    {:ok, _} = MockWatcher.start_link(mock)
+    :ok = GenServer.call(watcher, {:record_call, :fn_name, [1, 2]})
+    assert not GenServer.call(watcher, {:call_exists, :fn_name, [:other_args]})
+  end
+
+  test "should record and verify that wrong func call with correct args does not exist",
+  %{mock: mock, watcher: watcher} do
+    {:ok, _} = MockWatcher.start_link(mock)
+    :ok = GenServer.call(watcher, {:record_call, :fn_name, [:arg]})
+    assert not GenServer.call(watcher, {:call_exists, :other_fn, [:arg]})
+  end
+end
