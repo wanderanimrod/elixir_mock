635239bf148012fb60344138b7795961b569f5b1
Nimrod Wandera <wanderanimrod@gmail.com>
2017-04-03 21:02:36 -0400

    tag first usable version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mockex.Mixfile do
 
   def project do
     [app: :mockex,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
