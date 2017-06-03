efaba1cdb977e981f823d30cd39a4bee96ee8656
Nimrod Wandera <wanderanimrod@gmail.com>
2017-06-03 03:12:00 -0400

    Fixes mix version problem

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mockex.Mixfile do
 
   def project do
     [app: :mockex,
-     version: "0.2.1",
+     version: "0.2.2",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
